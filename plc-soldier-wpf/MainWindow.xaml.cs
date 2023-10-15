a16d594ab4e411bfe806d95058d4ab3dae52b82d
Disorol <49810312+Disorol@users.noreply.github.com>
2023-10-15 13:14:41 +0500

    Added functionality to the "Save as" and "Save" buttons

--- plc-soldier-wpf/MainWindow.xaml.cs
+++ plc-soldier-wpf/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,71 @@ namespace plc_soldier_wpf
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void SaveAsFileCommand(object sender, RoutedEventArgs e)
+        {
+            string fileText = TabItem1.IsSelected ? TextBox1.Text : TextBox2.Text;
+
+            SaveFileDialog dialog = new SaveFileDialog()
+            {
+                Filter = "Text Files(*.txt)|*.txt|All(*.*)|*"
+            };
+
+            if (dialog.ShowDialog() == true)
+            {
+                File.WriteAllText(dialog.FileName, fileText);
+
+                ApplicationState.SetValue("FileName", dialog.FileName);
+            }
+        }
+
+        public void SaveFileCommand(object sender, RoutedEventArgs e)
+        {
+            string fileText = TabItem1.IsSelected ? TextBox1.Text : TextBox2.Text;
+
+            SaveFileDialog dialog = new SaveFileDialog()
+            {
+                Filter = "Text Files(*.txt)|*.txt|All(*.*)|*"
+            };
+
+            if (ApplicationState.GetValue<string>("FileName") != null)
+            {
+                dialog.FileName = ApplicationState.GetValue<string>("FileName");
+
+                File.WriteAllText(dialog.FileName, fileText);
+            }
+            else if (dialog.ShowDialog() == true)
+            {
+                File.WriteAllText(dialog.FileName, fileText);
+
+                ApplicationState.SetValue("FileName", dialog.FileName);
+            }
+        }
+
+        public static class ApplicationState
+        {
+            private static Dictionary<string, object> _values =
+                       new Dictionary<string, object>();
+            public static void SetValue(string key, object value)
+            {
+                if (_values.ContainsKey(key))
+                {
+                    _values.Remove(key);
+                }
+                _values.Add(key, value);
+            }
+            public static T GetValue<T>(string key)
+            {
+                if (_values.ContainsKey(key))
+                {
+                    return (T)_values[key];
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
         }
     }
 }
